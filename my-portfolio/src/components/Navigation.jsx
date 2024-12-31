a9f7d7d90aa76157a7b8157057eaea1c3483de5b
Carrie Kam <satoshiminamisama@gmail.com>
2024-12-30 21:22:31 -0500

    [DEV] Fix hover border navigation

--- my-portfolio/src/components/Navigation.jsx
+++ my-portfolio/src/components/Navigation.jsx
@@ -17,10 +17,16 @@ const Navigation = () => {
         {navigationItems.map(({ name, icon }) => (
           <button 
             key={name} 
-            className="flex items-center px-3 py-2 rounded-md text-xl hover:bg-[#E3CAC6]"
-          >
+            className="flex items-center px-3 py-2 rounded-lg text-xl 
+                   relative overflow-hidden" 
+          > 
             <img src={icon} alt={name} className="w-10 h-10 mr-2" /> 
             {name}
+            <span 
+              className="absolute inset-0 border-[#A3466A] border-2 
+                       rounded-lg opacity-0 transition-opacity duration-100 
+                       hover:opacity-100"
+            ></span> 
           </button>
         ))}
       </div>

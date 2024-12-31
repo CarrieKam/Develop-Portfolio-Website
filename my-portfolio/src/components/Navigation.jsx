57c538164a82ffc32254c6069a46799218c1421c
Carrie Kam <satoshiminamisama@gmail.com>
2024-12-30 20:43:22 -0500

    [DEV] Add images in navigation bar

--- my-portfolio/src/components/Navigation.jsx
+++ my-portfolio/src/components/Navigation.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import aboutIcon from '../assets/logos/info.svg';
+import workIcon from '../assets/logos/work.svg';
+import projectsIcon from '../assets/logos/project.svg';
+import educationIcon from '../assets/logos/education.svg'; 
 
 const Navigation = () => {
-    return (
-      <nav className="fixed right-4 flex justify-between items-center p-4">
-        <div className="flex space-x-4">
-          {['About', 'Work', 'Projects', 'Education'].map((item) => (
-            <button key={item} className="px-3 py-2 rounded-md text-xl hover:bg-gray-700">
-              {item}
-            </button>
-          ))}
-        </div>
-      </nav>
-    );
-  };
-  export default Navigation;
+  const navigationItems = [
+    { name: 'About', icon: aboutIcon },
+    { name: 'Work', icon: workIcon },
+    { name: 'Projects', icon: projectsIcon },
+    { name: 'Education', icon: educationIcon },
+  ];
+
+  return (
+    <nav className="fixed right-4 flex justify-between items-center p-4">
+      <div className="flex space-x-4">
+        {navigationItems.map(({ name, icon }) => (
+          <button 
+            key={name} 
+            className="flex items-center px-3 py-2 rounded-md text-xl hover:bg-[#E3CAC6]"
+          >
+            <img src={icon} alt={name} className="w-10 h-10 mr-2" /> 
+            {name}
+          </button>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;

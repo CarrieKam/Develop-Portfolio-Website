273d195d7fb95caf7ff2e2aa433aca609768dca5
satoshiminamisama@gmail.com <satoshiminamisama@gmail.com>
2025-01-12 13:37:23 -0500

    [CSS] Fix how the cards behaves

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -22,12 +22,12 @@ const WorkTimeline = () => {
   useEffect(() => {
     // Import all images from the projects directory
     const images = import.meta.glob('../assets/projects/*.{png,jpg,jpeg,svg}', { eager: true });
-    
+
     // Map over projects and replace image paths with actual imports
     const projectsWithImages = data.projects.project.map(project => {
-      const imageName = project.imagePath.split('/').pop(); 
+      const imageName = project.imagePath.split('/').pop();
       const imagePath = imageName ? Object.entries(images).find(([path]) => path.includes(imageName)) : undefined;
-      
+
       return {
         ...project,
         imagePath: imagePath ? (imagePath[1] as { default: string }).default : project.imagePath
@@ -35,7 +35,7 @@ const WorkTimeline = () => {
     });
 
     // Group projects by category and sort by date within each category
-                          // reduce to group projects by their category
+                                                    // Acc = accumulator
     const groupedProjects = projectsWithImages.reduce((acc, project) => {
       if (!acc[project.category]) {
         acc[project.category] = [];
@@ -59,18 +59,19 @@ const WorkTimeline = () => {
                     // sub-component that renders individual project cards
                     // Takes a project object as a prop and renders the project details
   const ProjectCard = ({ project }: { project: typeof projectsByCategory[keyof typeof projectsByCategory][0] }) => (
-    <div className="flex items-center justify-center lg:min-h-[32.5rem] h-[25rem] sm:w-96 w-[80vw] mt-8">
+    <div className="flex items-center justify-center lg:min-h-[32.5rem] h-max sm:w-96 w-[80vw] md:mb-4">
       <BackgroundGradient className="p-4 mx-auto rounded-[22px] bg-white dark:bg-zinc-900">
         <div className="flex justify-center">
           <img src={project.imagePath} alt={project.title} className="w-64 h-40 object-cover rounded-lg" />
         </div>
-        <h3 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-2">{project.title}</h3>
-        <p className="text-lg">{project.date}</p>
+        <h3 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-2 sm:py-2">{project.title} ({project.date})</h3>
         <p className="text-lg">{project.description}</p>
         <br></br>
-        <a href={project.url} target="_blank" rel="noopener noreferrer" className="text-lg underline">
-          View project
-        </a>
+        <button className="px-8 py-1 border-x-slate-200 border-2 bg-white dark:bg-black relative group transition duration-200 text-black dark:text-white hover:bg-transparent hover:bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg">
+          <a href={project.url} target="_blank" rel="noopener noreferrer" className="text-lg">
+            View project
+          </a>
+        </button>
         <div className="flex flex-wrap gap-2 mt-4 justify-between items-center">
           {project.githubURL && (
             <a href={project.githubURL} target="_blank" rel="noopener noreferrer" className="text-lg underline">
@@ -94,11 +95,11 @@ const WorkTimeline = () => {
         <h2 className="text-4xl font-bold">Projects</h2>
       </div>
 
-      {/*Map over the grouped projects and render each category with its projects*/}
+      {/* Map over the grouped projects and render each category with its projects */}
       {Object.entries(projectsByCategory).map(([category, projects]) => (
-        <div key={category} className="mb-16">
-          <h3 className="text-3xl font-semibold mx-8">{category}</h3>
-          <div className="flex flex-wrap items-center justify-center p-4 gap-24 sm:mt-4">
+        <div key={category} className="md:mb-16 md:px-4 ">
+          <h3 className="text-3xl font-semibold mx-8 mb-8 md:mt-6">{category}</h3>
+          <div className="flex flex-wrap items-start justify-start p-4 md:mt-4 gap-8">
             {projects.map((project, index) => (
               <ProjectCard key={index} project={project} />
             ))}
